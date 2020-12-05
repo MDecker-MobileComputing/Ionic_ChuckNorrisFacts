f532ad933da5b895dcaaeac7bf972906cf41c85e
MDecker-MobileComputing <mide_42b@yahoo.de>
2020-12-05 15:21:55 +0100

    npm audit fix; HttpClientModule in app.module.ts dazu.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,23 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
+
+/**
+ * Für Verwendung von HttpClient muss unter `imports` der Eintrag
+ * `HttpClientModule` hinzugefügt werden; `HttpClientModule` muss
+ * natürlich auch importiert werden.
+ */
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    HttpClientModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
