be0e4e1e307fdf184992f7a178280243147e57c6
MDecker-MobileComputing <mide_42b@yahoo.de>
2020-12-05 18:21:12 +0100

    Error-Handler-Methode fuer Web-API-Request.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 
+/**
+ * Page zum Abruf einzelner Zufallswitze von Web-API von icndb.com.
+ */
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,17 +13,17 @@ import { HttpClient } from '@angular/common/http';
 export class HomePage {
 
   /** URL for REST endpoint of ICNDB's Web API. */
-  readonly URL_ICNDB = "https://api.icndb.com/jokes/random";
+  readonly URL_ICNDB = "https://api.icndb.com/jokes/random";  
 
+  /** Konfigurations-Objekt für HttpClient. */
   readonly OPTIONS_OBJECT: object = { observe: "response" };
 
-
   /** Member-Variable mit "Chuck Norris Fact", der mit Interpolation dargestellt wird. */
-  private witz = "asdf";
+  private witz = "";
 
 
   /**
-   * Constructor für Dependency Injection.
+   * Konstruktor für Dependency Injection.
    */
   constructor(private httpClient: HttpClient) {}
 
@@ -30,25 +33,42 @@ export class HomePage {
    */
   private onJokeLadenButton() {
 
-    const verarbeiteHttpResponse = (httpResponse:any) => {
-
-      console.log(`httpResponse: ${httpResponse.status}`);
-        
-      if (httpResponse.status === 200) {
-  
-        let joke = httpResponse.body.value.joke;
-
-        this.witz = joke.replace(/&quot\;/g, '"');
-        console.log(`httpResponse.body=${this.witz}`);
-  
-      } else {
-        
-        this.witz = `Fehler bei Zugriff auf Web-API: ${httpResponse.statusText} (${httpResponse.status})`;
-      }          
-    };
-
-    this.httpClient.get(this.URL_ICNDB, this.OPTIONS_OBJECT).subscribe(verarbeiteHttpResponse);
+    this.httpClient.get(this.URL_ICNDB, this.OPTIONS_OBJECT)
+                   .subscribe(this.verarbeiteHttpResponse, this.verarbeiteHttpFehler);
   }
 
+
+  /**
+   * Event-Handler für Verarbeitung von HTTP-Antwort.
+   * Mit Doppelpfeil-Operator definiert statt "normale" Methode, damit "this" auf Page-Objekt zeigt.
+   */
+  private verarbeiteHttpResponse = (httpResponse:any) => {
+
+    console.log(`httpResponse: ${httpResponse.status}`);
+        
+    if (httpResponse.status === 200) {
+
+      let joke = httpResponse.body.value.joke;
+
+      this.witz = joke.replace(/&quot\;/g, '"'); // &quot; mit Anführungszeichen ersetzen
+
+      console.log(`httpResponse.body=${this.witz}`);
+
+    } else {
+      
+      this.witz = `Fehler bei Zugriff auf Web-API: ${httpResponse.statusText} (${httpResponse.status})`;
+    }          
+  };
+
+
+  /**
+   * Fehlerbehandlungsmethode für HTTP-Request.
+   * 
+   * @param fehler  Objekt mit Details zu Fehler, u.a. `message`.
+   */
+  private verarbeiteHttpFehler = (fehler: HttpErrorResponse) => {
+
+    this.witz = "Fehler bei Abfrage Web-API von icndb.com: " + fehler.message;
+  }
 
 }
